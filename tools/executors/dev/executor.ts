5b8f55b7dd92d70154ba7359245b91e75cc51105
Pierre Huyghe <phuyghe.externe@bedrockstreaming.com>
2021-08-02 00:20:38 +0200

    wip

--- tools/executors/dev/executor.ts
+++ tools/executors/dev/executor.ts
@@ -1,5 +1,7 @@
 import { BuildExecutorSchema } from './schema';
 
+// cross-env RAZZLE_APP_PATH=basic-1
+
 export default async function runExecutor(options: BuildExecutorSchema) {
 
   console.log('Executor ran for Build', options);
